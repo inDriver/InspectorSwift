1b1e5a898f1ff687a058bcfdb2fbccd75f4cf9ce
Anton Goncharov <anton.goncharov@mybuddy.ai>
2024-04-02 15:36:05 +0300

    add XCFramework

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
-// swift-tools-version:5.4
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -8,38 +9,13 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(
-            name: "Inspector",
-            targets: ["Inspector"]
-        ),
-        .library(
-            name: "InspectorDynamic",
-            type: .dynamic,
-            targets: ["Inspector"]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ipedro/UIKeyCommandTableView.git", from: "0.3.3"),
-        .package(url: "https://github.com/ipedro/UIKeyboardAnimatable.git", from: "1.0.0"),
-        .package(url: "https://github.com/ipedro/UIKitOptions.git", from: "0.3.3"),
-        .package(url: "https://github.com/ipedro/Coordinator.git", from: "2.1.2")
+        .library(name: "Inspector", targets: ["Inspector"]),
     ],
     targets: [
-        .target(
-            name: "Inspector",
-            dependencies: [
-                "UIKeyCommandTableView",
-                "UIKeyboardAnimatable",
-                "UIKitOptions",
-                "Coordinator"
-            ],
-            resources: [
-                .process("Resources")
-            ]
-        ),
-        .testTarget(
-            name: "InspectorTests",
-            dependencies: ["Inspector"]
-        )
+        .binaryTarget(
+              name: "Inspector",
+              url: "https://github.com/inDriver/InspectorSwift/releases/download/3.3.0/InspectorSwift.xcframework.zip",
+              checksum: "18e376020613e65e889a09a365745e687025e063dde25f2b655c06c5271da789"
+            )
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import InspectorTests
-
-var tests = [XCTestCaseEntry]()
-tests += InspectorTests.allTests()
-XCTMain(tests)
